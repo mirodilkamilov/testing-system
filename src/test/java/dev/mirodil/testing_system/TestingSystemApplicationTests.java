73a894b86d12735297e155864f9caf24037b241a
mirodilkamilov <mirodilkamilov1999@gmail.com>
2025-01-06 12:56:35 +0100

    Spring Boot set up

---
+++ src/test/java/dev/mirodil/testing_system/TestingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mirodil.testing_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
