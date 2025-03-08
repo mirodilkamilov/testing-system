9357318780382714036cd932c23be6040a203eea
mirodilkamilov <mirodilkamilov1999@gmail.com>
2025-03-08 15:05:49 +0100

    Bug fix: make path key in all default Spring error response full URL instead of URI

--- src/main/java/dev/mirodil/testing_system/configs/GlobalErrorAttributes.java
+++ src/main/java/dev/mirodil/testing_system/configs/GlobalErrorAttributes.java
@@ -1,8 +1,10 @@
 package dev.mirodil.testing_system.configs;
 
+import org.apache.catalina.connector.RequestFacade;
 import org.springframework.boot.web.error.ErrorAttributeOptions;
 import org.springframework.boot.web.servlet.error.DefaultErrorAttributes;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.ServletWebRequest;
 import org.springframework.web.context.request.WebRequest;
 
 import java.time.Instant;
@@ -20,6 +22,14 @@ public class GlobalErrorAttributes extends DefaultErrorAttributes {
         // Convert timestamp to Instant in UTC (second precision)
         String timestamp = DateTimeFormatter.ISO_INSTANT.format(Instant.now().truncatedTo(ChronoUnit.SECONDS));
         errorAttributes.put("timestamp", timestamp);
+
+        // Convert path URI to full URL path
+        RequestFacade requestFacade = ((ServletWebRequest) webRequest).getNativeRequest(RequestFacade.class);
+        if (requestFacade != null) {
+            String fullUrl = requestFacade.getRequestURL().toString();
+            errorAttributes.put("path", fullUrl);
+        }
+
 
         return errorAttributes;
     }
